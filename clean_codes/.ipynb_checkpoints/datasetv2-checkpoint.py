732a887bb9cd2b056c78ce1d1559878e2cc0acce
Brenjit <jan2maina28@gmail.com>
2026-04-20 12:18:54 +0530

    datasetv2.py added

---
+++ clean_codes/.ipynb_checkpoints/datasetv2-checkpoint.py
@@ -0,0 +1,68 @@
+import os
+import glob
+import numpy as np
+import torch
+from torch.utils.data import Dataset
+import torch.nn.functional as F
+
+class LightCurveDataset(Dataset):
+    def __init__(self, folder: str, data_type: str,device: str):
+        # self.lc_folder = os.path.join(folder, "LC10")
+        # self.img_folder = os.path.join(folder, "OM10")
+        self.lc_folder = os.path.join(folder)
+        self.img_folder = os.path.join(folder)
+        
+        self.device=device
+        self.data_type = data_type
+        
+        
+        self.base_names = []
+        lc_files_paths = sorted(glob.glob(os.path.join(self.lc_folder, f"{self.data_type}_*LC.npy")))
+        
+        for lc_path in lc_files_paths:
+            base_name = os.path.basename(lc_path).replace('LC.npy', '')
+            
+            self.base_names.append(base_name)
+
+        self.lc_paths = [os.path.join(self.lc_folder, f"{name}LC.npy") for name in self.base_names]
+        #self.depth_paths = [os.path.join(self.lc_folder, f"{name}_meta.npy") for name in self.base_names]
+        
+        
+        self.img_paths = [os.path.join(self.img_folder, f"{name.strip()}.npy") for name in self.base_names]
+        
+        
+        for i, path in enumerate(self.img_paths):
+            if not os.path.exists(path):
+                no_space_path = os.path.join(self.img_folder, f"{self.base_names[i].strip()}.npy")
+                if os.path.exists(no_space_path):
+                    self.img_paths[i] = no_space_path
+                else:
+                    raise FileNotFoundError(f"Could not find image file for {self.base_names[i]}: {path} or {no_space_path}")
+
+        self.lc_counts = [np.load(path, mmap_mode='r').shape[0] for path in self.lc_paths]
+        self.cumulative_counts = np.cumsum(self.lc_counts)
+        self.total_samples = self.cumulative_counts[-1] if self.cumulative_counts.size > 0 else 0
+        self.lc_data = []
+        self.depth_data = []
+        self.img_data = []
+
+        for lc_path, img_path in zip(self.lc_paths, self.img_paths):
+            self.lc_data.append(np.load(lc_path))
+            #self.depth_data.append(np.load(depth_path))
+            self.img_data.append(np.load(img_path))
+
+        self.lc_data =    torch.tensor(np.concatenate(self.lc_data), dtype=torch.float32).to(self.device)
+        #self.depth_data = torch.tensor(np.concatenate(self.depth_data), dtype=torch.float32).to(self.device)
+        #self.depth_data[:,-1]=1/self.depth_data[:,-1]
+        self.img_data =   torch.tensor(np.concatenate(self.img_data), dtype=torch.float32).to(self.device)
+        print(f'for lc max: {self.lc_data.max()} , min: {self.lc_data.min()}')
+        #print(f'for metadata max: {self.depth_data.max()} , min: {self.depth_data.min()}')
+
+    def __len__(self):
+        return len(self.lc_data)
+
+    def __getitem__(self, idx: int):
+        lc_tensor = self.lc_data[idx].unsqueeze(0)
+        #depth_tensor = self.depth_data[idx]
+        img_tensor = self.img_data[idx].unsqueeze(0)
+        return lc_tensor,0, img_tensor
---
+++ clean_codes/train_on_kepler_noise.py
@@ -0,0 +1,178 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import json
+from torch.utils.data import DataLoader
+import numpy as np
+from datasetv2 import *
+#from models2 import *
+from models import *
+import cProfile
+import pstats
+from utils import *
+import matplotlib.pyplot as plt
+# In main.py
+#criterion = nn.MSELoss()
+criterionb = symmetry_aware_bce
+
+criterion = symmetry_aware_dice_loss
+rng = np.random.default_rng(seed=42) # Optional: provide a seed for reproducibility
+figpath = f"/home/iit-t/Gitika/Github-Repositories/Abraham_Mega/Reanalysis_Git/Mega_PartII_Kepler/plots/debug_sigmoid_epoch"
+def train_gan(generator, traindataloader, valdataloader, snr, num_epochs=50, device="cuda",modelpath='Linear',n=1):
+    optimizer_G = optim.AdamW(generator.parameters(), lr=1e-3, weight_decay=1e-8)
+    scheduler = optim.lr_scheduler.ReduceLROnPlateau(optimizer_G, 'min', patience=15, factor=0.5)
+    best_val_loss = float("inf") # Simplified this
+    val_loss_counter = 0
+
+    for epoch in range(num_epochs):
+        generator.train()
+        gbce_total = 0
+        gmse_total = 0
+        # if epoch>100:
+        #     snru=50+450*rng.random()
+        # elif epoch>60:
+        #     snru=250+250*rng.random()
+        # elif epoch>30:
+        #     snru=350+150*rng.random()
+        # else:
+        #     snru=400+100*rng.random()
+        for i, (lc_batch, real_depths, real_imgs) in enumerate(traindataloader):
+            # noisy_lc = add_noise_to_batch(lc_batch.squeeze(1), snru)
+            # #print(noisy_lc.shape)
+            # optimizer_G.zero_grad()
+            # gen_imgs = generator(noisy_lc.view(noisy_lc.shape[0],1,120))
+
+            lc_input = lc_batch.squeeze(1)
+            optimizer_G.zero_grad()
+            gen_imgs = generator(lc_input.view(lc_input.shape[0], 1, 120))
+            
+            g_loss_bce = symmetry_aware_bce(real_imgs.squeeze(),gen_imgs.squeeze()) 
+            
+            g_loss_mse = symmetry_aware_mse(real_imgs.squeeze(),gen_imgs.squeeze())
+            if epoch>60:
+                genlos = g_loss_bce
+            else:
+                genlos = g_loss_bce
+            
+            gmse_total += g_loss_mse.detach()
+            gbce_total += g_loss_bce.detach()
+            
+            genlos.backward()
+            if i == 1 and epoch%10==0:
+                print_grad_stats(generator, i)
+            optimizer_G.step()
+            if i == 0 and epoch % 10 == 0:
+                
+
+                prob_map = gen_imgs[0].squeeze().detach().cpu().numpy()
+                true_img = real_imgs[0].squeeze().detach().cpu().numpy()
+
+                hard_shape = (prob_map > 0.5).astype(float)
+
+                plt.figure(figsize=(12, 4))
+
+                plt.subplot(1, 3, 1)
+                plt.title("Ground Truth")
+                plt.imshow(true_img, vmin=0, vmax=1, cmap='inferno')
+                plt.axis('off')
+
+                plt.subplot(1, 3, 2)
+                plt.title(f"Sigmoid Output (Confidence)\nMSE: {g_loss_mse.item():.4f}")
+                plt.imshow(prob_map, vmin=0, vmax=1, cmap='inferno')
+                plt.axis('off')
+
+                plt.subplot(1, 3, 3)
+                plt.title("Thresholded > 0.5\n(Final Prediction)")
+                plt.imshow(hard_shape, vmin=0, vmax=1, cmap='inferno')
+                plt.axis('off')
+
+                #plt.savefig(f"plots/debug_sigmoid_epoch_{epoch}.png")
+                plt.savefig(f"{figpath}_{epoch}.png")
+                plt.close()
+
+        avg_train_bce = gbce_total / len(traindataloader)
+        avg_train_mse = gmse_total / len(traindataloader)
+        
+        val_bce_losses = []
+        val_mse_losses = []
+        val_total_losses = []
+        
+        generator.eval() 
+        with torch.no_grad():
+            for lc_batch, real_depths, real_imgs in valdataloader:
+                # noisy_lc = add_noise_to_batch(lc_batch.squeeze(1), snru)
+                # gen_imgs = generator(noisy_lc.view(noisy_lc.shape[0],1,120))
+
+                lc_input = lc_batch.squeeze(1)
+                gen_imgs = generator(lc_input.view(lc_input.shape[0], 1, 120))
+            
+                val_bce = symmetry_aware_bce(real_imgs.squeeze(), gen_imgs.squeeze())
+                val_mse = symmetry_aware_mse(real_imgs.squeeze(), gen_imgs.squeeze())
+                
+                val_bce_losses.append(val_bce.item())
+                val_mse_losses.append(val_mse.item())
+                if epoch>60:
+                    val_total_losses.append(val_mse.item()+val_bce.item())
+                else:
+                    val_total_losses.append(val_bce.item()+val_mse.item())
+        
+        avg_val_bce = np.mean(val_bce_losses)
+        avg_val_mse = np.mean(val_mse_losses)
+        avg_val_loss = np.mean(val_total_losses) 
+        old_lr = optimizer_G.param_groups[0]['lr'] 
+        
+        scheduler.step(avg_val_loss)
+        
+        new_lr = optimizer_G.param_groups[0]['lr']        
+        if new_lr < old_lr:
+            print(f"✨ Learning rate reduced to {new_lr}. Loading best model from checkpoint.")
+            generator.load_state_dict(torch.load(modelpath,weights_only=True))
+        
+        if avg_val_loss < best_val_loss:
+            best_val_loss = avg_val_loss
+            val_loss_counter = 0
+            torch.save(generator.state_dict(), modelpath)
+        else:
+            val_loss_counter += 1
+        
+        if val_loss_counter > 80:
+            print("Validation loss has not improved for 20 epochs. Early stopping.")
+            break
+
+        if (epoch + 1) % 1 == 0 or epoch == 0:
+            print(f"{epoch+1}/{num_epochs}] Train BCE: {avg_train_bce:.4f}, Train MSE: {avg_train_mse:.4f} | Val BCE: {avg_val_bce:.4f}, Val MSE: {avg_val_mse:.4f}")
+            
+    print(f"✔ Finished training for SNR={snr}, best val loss={best_val_loss:.4f}")
+
+if __name__ == "__main__":
+    import argparse
+    parser = argparse.ArgumentParser(description="Train GAN progressively from high to low SNR.")
+    parser.add_argument("--data", type=str, default="/home/iit-t/Gitika/Github-Repositories/Abraham_Mega/Reanalysis_Git/Mega_PartII_Kepler/Data/", help="Path to the data directory.")
+    parser.add_argument("--epochs", type=int, default=10, help="Epochs per SNR")
+    parser.add_argument("--batch-size", type=int, default=32, help="Batch size for training")
+    parser.add_argument("--n", type=int, default=2, help="scaling controll")
+    parser.add_argument("--device", type=str, default="cuda" if torch.cuda.is_available() else "cpu", help="Training device")
+    parser.add_argument("--modelpath", type=str, required=True, help=f'/home/iit-t/Gitika/Github-Repositories/Abraham_Mega/Reanalysis_Git/Mega_PartII_Kepler/models/mo4{args.n}.pth')
+    args = parser.parse_args()
+    print("args.data",args.data)
+    traindataset = LightCurveDataset(args.data, 'train',device=args.device)
+    print('traindataset',traindataset)
+    valdataset = LightCurveDataset(args.data, 'val',device=args.device)
+    print(f"Total number of training samples: {len(traindataset)}")
+    print(f"Total number of validation samples: {len(valdataset)}")
+
+    traindataloader = DataLoader(traindataset, batch_size=args.batch_size, shuffle=True, num_workers=0)
+    valdataloader = DataLoader(valdataset, batch_size=args.batch_size, shuffle=False, num_workers=0)
+
+    latent_dim = 120
+    n = args.n
+
+    generator = HybridConvNet(n=n)
+    generator.to(args.device)
+    #modelpath=f'models/mo4{n}.pth
+    #modelpath=f'/home/iit-t/Gitika/Github-Repositories/Abraham_Mega/Reanalysis_Git/Mega_PartII_Kepler/models/mo4{n}.pth'
+    
+    
+    
+    train_gan(generator, traindataloader, valdataloader, 500, num_epochs=args.epochs, device=args.device,modelpath=modelpath,n=n)
+    #train_gan(generator, traindataloader, valdataloader, 50, num_epochs=args.epochs, device=args.device,modelpath=modelpath,n=n)
