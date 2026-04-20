cc5de78f663556197dcb33e03c410a9d62a26cc0
Brenjit <jan2maina28@gmail.com>
2026-04-20 13:06:11 +0530

    datasetv2.py added

--- clean_codes/.ipynb_checkpoints/train_on_kepler_noise-checkpoint.py
+++ clean_codes/.ipynb_checkpoints/train_on_kepler_noise-checkpoint.py
@@ -152,7 +152,7 @@ if __name__ == "__main__":
     parser.add_argument("--batch-size", type=int, default=32, help="Batch size for training")
     parser.add_argument("--n", type=int, default=2, help="scaling controll")
     parser.add_argument("--device", type=str, default="cuda" if torch.cuda.is_available() else "cpu", help="Training device")
-
+    parser.add_argument("--modelpath", type=str, required=True, help=f'/home/iit-t/Gitika/Github-Repositories/Abraham_Mega/Reanalysis_Git/Mega_PartII_Kepler/models/)
     args = parser.parse_args()
     print("args.data",args.data)
     traindataset = LightCurveDataset(args.data, 'train',device=args.device)
@@ -166,13 +166,13 @@ if __name__ == "__main__":
 
     latent_dim = 120
     n = args.n
-    parser.add_argument("--modelpath", type=str, required=True, help=f'/home/iit-t/Gitika/Github-Repositories/Abraham_Mega/Reanalysis_Git/Mega_PartII_Kepler/models/mo4{n}.pth')
+
     generator = HybridConvNet(n=n)
     generator.to(args.device)
     #modelpath=f'models/mo4{n}.pth
     #modelpath=f'/home/iit-t/Gitika/Github-Repositories/Abraham_Mega/Reanalysis_Git/Mega_PartII_Kepler/models/mo4{n}.pth'
-    
-    
+    modelpath = args.modelpath
+    modelpath = f"{modelpath}mo4{n}.pth'"
     
     train_gan(generator, traindataloader, valdataloader, 500, num_epochs=args.epochs, device=args.device,modelpath=modelpath,n=n)
     #train_gan(generator, traindataloader, valdataloader, 50, num_epochs=args.epochs, device=args.device,modelpath=modelpath,n=n)
