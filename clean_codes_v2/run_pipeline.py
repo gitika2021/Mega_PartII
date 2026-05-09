4a0e68361a4513f63709c8a65b97408cdbf893c9
a-paranjape <aseem@iucaa.in>
2026-05-09 15:20:33 +0530

    updated config

--- clean_codes_v2/run_pipeline.py
+++ clean_codes_v2/run_pipeline.py
@@ -2,40 +2,58 @@ import numpy as np
 import sys
 from class_modules import MLPreProcessing
 import train_on_kepler_noise
+from paths import Config_Dir
+import argparse,json
 
 if __name__ == "__main__":
-    # train = bool(sys.argv[1])
-    # Num = int(sys.argv[2])
-    # N = int(sys.argv[3]) if len(sys.argv) > 3 else 1
-    # maps_path = sys.argv[4] if len(sys.argv) > 4 else None
-    # nproc = int(sys.argv[5]) if len(sys.argv) > 5 else 4
-    # # train_frac and seed should also be in user control
+    # train = True
+    # Num = 10
+    # N = 1
+    # maps_path = None
+    # nproc = 4
+    # rsrp1 = 5
+    # rsrp2 = 10
+    # train_frac = 0.8
+    # seed = None
 
-    # rsrp1 = int(sys.argv[4])
-    # rsrp2 = int(sys.argv[5])
-    # koi_table_folder = int(sys.argv[6])
-    # koi_table_filename = int(sys.argv[7])
+    # # only needed for actual training
+    # epochs = 3
+    # batch_size = 10 # 32
+    # n_scale = 2
+    # device = 'cpu' # "cuda" if torch.cuda.is_available() else "cpu"
+    # resume = False
+    # checkpoint_freq = 3
 
-    train = True
-    Num = 10
-    N = 1
-    maps_path = None
-    nproc = 4
-    rsrp1 = 5
-    rsrp2 = 10
-    train_frac = 0.8
-    seed = None
+    parser = argparse.ArgumentParser(description="Lightcurve generation and model training.")
+    parser.add_argument("--config-file",type=str,required=True, help="name of config file (e.g., example_config.json)")
+    parser.add_argument("--train",type=bool,help="whether to implement training (True) or pre-processing (False).")
+    parser.add_argument("--N",type=int,help="index number for pre-processing batch.")
+    args = parser.parse_args()
 
-    # only needed for actual training
-    epochs = 3
-    batch_size = 10 # 32
-    n_scale = 2
-    device = 'cpu' # "cuda" if torch.cuda.is_available() else "cpu"
-    resume = False
-    checkpoint_freq = 3
+    config_file = args.config_file
+    with open(Config_Dir+config_file,'r') as f:
+        config = json.load(f)
+    
+    train = args.train if args.train is not None else config['train']
+    N = args.N if args.N is not None else config['N']
+
+    Num = config.get('Num',10)
+    maps_path = config.get('maps_path',None)
+    nproc = config.get('nproc',4)
+    rsrp1 = config.get('rsrp1',5)
+    rsrp2 = config.get('rsrp2',10)
+    train_frac = config.get('train_frac',0.8)
+    seed = config.get('seed',None)
     
     obj = MLPreProcessing(Num=Num,N=N,maps_path=maps_path,nproc=nproc,rsrp1=rsrp1,rsrp2=rsrp2,train_frac=train_frac,seed=seed)
     if train:
+        import torch
+        epochs = config.get('epochs',3)
+        batch_size = config.get('batch_size',10)
+        n_scale = config.get('n_scale',2)
+        device = "cuda" if torch.cuda.is_available() else "cpu"
+        resume = config.get('resume',False)
+        checkpoint_freq = config.get('checkpoint_freq',3)
         train_on_kepler_noise.main(obj.train_dir,obj.model_dir,
                                    epochs,batch_size,n_scale,device,resume,checkpoint_freq,figpath=str(obj.figure_dir))
     else:
