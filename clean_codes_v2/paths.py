42abc3c92d26eca19f2a44c3e191bd0f26ba0297
Gitika Shukla (Prof. Vikram Khaire) <cshukla.gitika@ln4.cm.cluster>
2026-05-27 14:02:54 +0530

    updates

--- clean_codes_v2/paths.py
+++ clean_codes_v2/paths.py
@@ -1,4 +1,4 @@
-
+'''
 
 # IITT Workstation Paths
 Home = '/home/iit-t/Gitika/Github-Repositories/Abraham_Mega/Reanalysis_Git/Mega_PartII_Kepler/Mega_PartII/'
@@ -7,18 +7,16 @@ Kepler_Dir = Home+'Kepler/'
 Config_Dir = Home+'Config/'
 KOI_Table_Filename = 'koi_cumulative_2025.06.28_01.24.15.csv'
 Kepler_Error_Filename = 'kepler_folded_lcs_snr50_all_binned_err.npy'
-#Infer_LC_Dir = Home+"Raw_LC/"
-Infer_LC_Dir = Base_Dir+"Kepler_RsRp_Bins/RsRp_2_5/"
-
+Infer_LC_Dir = Home+"Raw_LC/"
 '''
 
 # Pegasus Paths
-Home = '/mnt/home/project/cshukla.gitika/Mega_PartII_ref2/'
-Base_Dir = Home+'Mega_Runs_Pegasus/'
+Home = '/mnt/home/project/cshukla.gitika/Mega_PartII/'
+Base_Dir = Home+'Test_Runs_Pegasus/'
+#Base_Dir = Home+'Mega_Runs_Pegasus/'
 Kepler_Dir = Home+'Kepler/'
 Config_Dir = Home+'Config/'
 KOI_Table_Filename = 'koi_cumulative_2025.06.28_01.24.15.csv'
 Kepler_Error_Filename = 'kepler_folded_lcs_snr50_all_binned_err.npy'
-#Infer_LC_Dir = Home+"Raw_LC/"
-Infer_LC_Dir = Base_Dir+"Kepler_RsRp_Bins/RsRp_2_5/"
-'''
+Infer_LC_Dir = Home+"Raw_LC/"
+
