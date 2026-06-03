21302d46a76ea7586a767d7c16ad205104fd83df
Gitika Shukla (Prof. Vikram Khaire) <cshukla.gitika@ln4.cm.cluster>
2026-06-03 12:17:23 +0530

    updated

--- clean_codes_v2/paths.py
+++ clean_codes_v2/paths.py
@@ -1,4 +1,4 @@
-
+'''
 
 # IITT Workstation Paths
 Home = '/home/iit-t/Gitika/Github-Repositories/Abraham_Mega/Reanalysis_Git/Mega_PartII_Kepler/Mega_PartII/'
@@ -22,4 +22,3 @@ KOI_Table_Filename = 'koi_cumulative_2025.06.28_01.24.15.csv'
 Kepler_Error_Filename = 'kepler_folded_lcs_snr50_all_binned_err.npy'
 #Infer_LC_Dir = Home+"Raw_LC/"
 Infer_LC_Dir = Base_Dir+"Kepler_RsRp_Bins/"
-'''
