577f0e84aa5c9d263fab524579b281a3fdb5287c
Gitika Shukla (Prof. Vikram Khaire) <cshukla.gitika@ln4.cm.cluster>
2026-06-15 14:37:46 +0530

    added

--- PBS_Scripts/rsrp_3_3_cpu_quick_sync.sh
+++ PBS_Scripts/rsrp_3_3_cpu_quick_sync.sh
@@ -21,62 +21,42 @@ PBS_JOBNAME3="genlc_${ratio1}_${ratio2}_s3"
 # PBS_JOBNAME5="genlc_${ratio1}_${ratio2}_s5"
 PBS_JOBNAME6="train_${ratio1}_${ratio2}"
 
-# jid0=$(qsub -v ratio1=$ratio1,ratio2=$ratio2,seed=$seed,base_dir=$base_dir \
-#     -q $queue \
-#     -N $PBS_JOBNAME0 \
-#     gene_config_quick.pbs)
-
-# jid1=$(qsub \
-#     -q $queue \
-#     -l walltime=$walltime \
-#     -W depend=afterok:${jid0} \
-#     -v ratio1=$ratio1,ratio2=$ratio2,seed=$seed,base_dir=$base_dir \
-#     -N $PBS_JOBNAME1 \
-#     gene_data_set1.pbs)
-
-# jid2=$(qsub \
-#     -q $queue \
-#     -l walltime=$walltime \
-#     -W depend=afterok:${jid0} \
-#     -v ratio1=$ratio1,ratio2=$ratio2,seed=$seed,base_dir=$base_dir \
-#     -N $PBS_JOBNAME2 \
-#     gene_data_set2.pbs)
-
-# jid3=$(qsub \
-#     -q $queue \
-#     -l walltime=$walltime \
-#     -W depend=afterok:${jid0} \
-#     -v ratio1=$ratio1,ratio2=$ratio2,seed=$seed,base_dir=$base_dir \
-#     -N $PBS_JOBNAME3 \
-#     gene_data_set3.pbs)
-
-# qsub \
-#     -q $queue \
-#     -l walltime=$walltime \
-#     -W depend=afterok:${jid1}:${jid2}:${jid3} \
-#     -v ratio1=$ratio1,ratio2=$ratio2,base_dir=$base_dir \
-#     -N $PBS_JOBNAME6 \
-#     train_cpu.pbs
-
 jid0=$(qsub -v ratio1=$ratio1,ratio2=$ratio2,seed=$seed,base_dir=$base_dir \
     -q $queue \
     -N $PBS_JOBNAME0 \
     gene_config_quick.pbs)
 
-# jid1=$(qsub \
-#     -q $queue \
-#     -l walltime=$walltime \
-#     -v ratio1=$ratio1,ratio2=$ratio2,seed=$seed,base_dir=$base_dir \
-#     -N $PBS_JOBNAME1 \
-#     gene_data_set1.pbs)
-    
+jid1=$(qsub \
+    -q $queue \
+    -l walltime=$walltime \
+    -W depend=afterok:${jid0} \
+    -v ratio1=$ratio1,ratio2=$ratio2,seed=$seed,base_dir=$base_dir \
+    -N $PBS_JOBNAME1 \
+    gene_data_set1.pbs)
+
+jid2=$(qsub \
+    -q $queue \
+    -l walltime=$walltime \
+    -W depend=afterok:${jid0} \
+    -v ratio1=$ratio1,ratio2=$ratio2,seed=$seed,base_dir=$base_dir \
+    -N $PBS_JOBNAME2 \
+    gene_data_set2.pbs)
+
+jid3=$(qsub \
+    -q $queue \
+    -l walltime=$walltime \
+    -W depend=afterok:${jid0} \
+    -v ratio1=$ratio1,ratio2=$ratio2,seed=$seed,base_dir=$base_dir \
+    -N $PBS_JOBNAME3 \
+    gene_data_set3.pbs)
+
 qsub \
     -q $queue \
     -l walltime=$walltime \
-    -W depend=afterok:${jid0} \    
+    -W depend=afterok:${jid1}:${jid2}:${jid3} \
     -v ratio1=$ratio1,ratio2=$ratio2,base_dir=$base_dir \
     -N $PBS_JOBNAME6 \
     train_cpu.pbs
-    
+
 end_time=$(date +%s)
 echo "Training Job Submitted"
